458f648c6e5ea0ec19835668a8aa9a8ff39444e4
Kenneth Lim <kennethlimcp@gmail.com>
2014-10-05 10:04:16 +0800

    Initial Commit

---
+++ firmware/examples/stepper-demo.ino
@@ -0,0 +1,41 @@
+//Includes
+#include "Stepper/Stepper.h"
+#include "math.h"
+
+//Defines
+#define STEPS 200
+#define ADCwidth 12
+#define ADCsteps pow(2, ADCwidth)
+#define maxSpeed 256 //RPM (for test, using max LED value 255)
+
+Stepper stepper1(STEPS, D0, D1, D2, D3);
+
+int potPin = A0;
+int ledPin = D5;
+int potVal = 0;
+uint8_t speed = 0;
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(potPin, INPUT);
+    pinMode(ledPin, OUTPUT); //LED brightness correlates to motor speed
+    stepper1.setSpeed(0);
+}
+
+void loop() {
+    potVal = analogRead(potPin);
+    speed = floor(potVal/(ADCsteps/maxSpeed));
+    analogWrite(ledPin, speed);
+    stepper1.setSpeed(speed);
+    stepper1.step(1);
+
+    display();
+}
+
+void display() {
+    Serial.print("Pot Val: ");
+    Serial.print(potVal);
+    Serial.print(" | ");
+    Serial.print("Speed: ");
+    Serial.println(speed);
+}
